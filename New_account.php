13c2307a20c7173cc073a6356ed4e5c41f450378
ReddyRaghu04 <raghu.reddy129760@marwadiuniversity.ac.in>
2025-04-01 00:40:49 +0530

    Update

--- New_account.php
+++ New_account.php
@@ -79,7 +79,13 @@
         </div>
 
         <div id="bank" style="display: none;" class="form">
-            <h3>Bank Details</h3>
+            <h3 style="display: flex; align-items: center; justify-content: center;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bank" viewBox="0 0 16 16" style="margin-right: 10px;">
+                    <path d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.5.5 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89zM3.777 3h8.447L8 1zM2 6v7h1V6zm2 0v7h2.5V6zm3.5 0v7h1V6zm2 0v7H12V6zM13 6v7h1V6zm2-1V4H1v1zm-.39 9H1.39l-.25 1h13.72z"/>
+                </svg>
+             Bank Details
+            </h3>
+
             <label class="form-label">Bank Account Holder Name</label><br>
             <input type="text" name="bank_holder_name"><br>
             <label class="form-label">Bank Account Number</label><br>
